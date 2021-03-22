c7547ebb9cade2cc7de586d92dfa4cfeadf694b1
Olivia Neil-Jones <olivianeiljones@gmail.com>
2021-03-22 18:42:16 +0100

    first day of work

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import { setConstantValue } from 'typescript';
 import './App.css';
 
+interface IPasteBin {
+  id: number,
+  title: string,
+  description: string,
+  language: string
+}
+
 function App() {
+  const emptyPasteBin: IPasteBin[] = []
+  const [pasteBin, setPasteBin] = useState(emptyPasteBin)
+
+  async function handleAddSnippet() {
+    const res = await fetch(`http://localhost:4000/`, {
+      method: "post",
+      headers: { "Content-Type": "application/json" }, //need this to be read by app(express.json())
+      body: JSON.stringify({
+        //data needs to be SERIALISED to make the journey
+        description: , //set the code snippet
+      }),
+    });
+
+  // }
+  
+  // async function handleGetSnippet() {
+  //   const res = await fetch(`http://localhost:4000/:id`);
+  //   setSnippet(await res.json());
+  // }
+  
+  async function handleGetAll() {
+        const res = await fetch(`http://localhost:4000/`);
+        setPasteBin(await res.json());
+        console.log(pasteBin)
+        // console.log(res)
+        // const res1= await res.json(); //what is this actually doing??
+        // console.log(res1) //all we need to know: it's an array of objects!!
+  }
+
+  useEffect( () => {
+    handleGetAll()
+  },[])
+
+  function SeeSnippets(props: IPasteBin) {
+    return(
+      <ul> {props.id} {props.title} </ul>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='input_box'> 
+      <input
+        placeholder='insert title here!'
+      ></input>
+      <textarea
+        placeholder='paste code snippet here!'
+        onChange={}
+      ></textarea>
+      <select> 
+        <option value='JS'> JS </option>
+        <option value='TS'> TS </option>
+        <option value='Python'> Python </option>
+      </select>
+      <button onClick={() => {handleGetAll(); handleAddSnippet()}}> SUBMIT </button>
+      </div>
+
+      <div className='list'>
+        <li>
+          {pasteBin.map((element, index) => <SeeSnippets {...element} key={index} />)}
+        </li>
+
+      </div>
     </div>
   );
 }
+
+
+
 
 export default App;
