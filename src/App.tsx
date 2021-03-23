a90ec804151cedbf3ea850421398762966b4bff6
Muhammad Tanvir Bin Ali <ali2912@outlook.com>
2021-03-23 15:57:15 +0000

    MVP with edit and delete prototypes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { setConstantValue } from 'typescript';
 import './App.css';
 
@@ -11,7 +11,13 @@ interface IPasteBin {
 
 function App() {
   const emptyPasteBin: IPasteBin[] = []
+  const emptyChosenSnippet = {} as IPasteBin
+  // const emptySnippet: string
   const [pasteBin, setPasteBin] = useState(emptyPasteBin)
+  const [newDescription, setNewDescription] = useState("")
+  const [newTitle, setNewTitle] = useState("")
+  const [newLanguage, setNewLanguage] = useState("")
+  const [chosenSnippet, setChosenSnippet] = useState(emptyChosenSnippet)
 
   async function handleAddSnippet() {
     const res = await fetch(`http://localhost:4000/`, {
@@ -19,52 +25,97 @@ function App() {
       headers: { "Content-Type": "application/json" }, //need this to be read by app(express.json())
       body: JSON.stringify({
         //data needs to be SERIALISED to make the journey
-        description: , //set the code snippet
+        description: newDescription, //set the code snippet
+        title: newTitle,
+        language: newLanguage,
       }),
-    });
+    })
+    handleGetAll()
+    setNewTitle("")
+    setNewLanguage("")
+  };
 
   // }
-  
+
   // async function handleGetSnippet() {
   //   const res = await fetch(`http://localhost:4000/:id`);
   //   setSnippet(await res.json());
   // }
-  
+
   async function handleGetAll() {
-        const res = await fetch(`http://localhost:4000/`);
-        setPasteBin(await res.json());
-        console.log(pasteBin)
-        // console.log(res)
-        // const res1= await res.json(); //what is this actually doing??
-        // console.log(res1) //all we need to know: it's an array of objects!!
+    const res = await fetch(`http://localhost:4000/`);
+    setPasteBin(await res.json());
+    console.log(pasteBin)
+    // console.log(res)
+    // const res1= await res.json(); //what is this actually doing??
+    // console.log(res1) //all we need to know: it's an array of objects!!
   }
 
-  useEffect( () => {
+  useEffect(() => {
     handleGetAll()
-  },[])
+  }, [])
+
+  async function handleDeleteSnippet(props: IPasteBin) {
+    const res = await fetch(`http://localhost:4000/` + props.id.toString(), {
+      method: "delete",
+      headers: { "Content-Type": "application/json" }, //need this to be read by app(express.json())
+      body: JSON.stringify({
+        //data needs to be SERIALISED to make the journey
+        id: props.id
+      }),
+    })
+    handleGetAll()
+  };
+
+  async function handleEditSnippet(props: IPasteBin) {
+    //set state to selected id
+    setNewTitle(props.title)
+    setNewDescription(props.description)
+    setNewLanguage(props.language)
+    const res = await fetch(`http://localhost:4000/` + props.id.toString(), {
+      method: "put",
+      headers: { "Content-Type": "application/json" }, //need this to be read by app(express.json())
+      body: JSON.stringify({
+        id: props.id,
+        title: "editTitleTest",
+        description: "editDescritpionTest",
+        language: "editLanguageTest"
+      }),
+    })
+    handleGetAll()
+  };
 
   function SeeSnippets(props: IPasteBin) {
-    return(
-      <ul> {props.id} {props.title} </ul>
+
+    return (
+      <ul> {props.id} {props.description}
+        <button onClick={() => { handleDeleteSnippet(props) }}>Delete Snippet</button>
+        <button onClick={() => { setChosenSnippet(props) }}>See Snippet</button>
+        <button onClick={() => { handleEditSnippet(props) }}>Edit Snippet</button> </ul>
     )
+
   }
 
   return (
     <div className="App">
-      <div className='input_box'> 
-      <input
-        placeholder='insert title here!'
-      ></input>
-      <textarea
-        placeholder='paste code snippet here!'
-        onChange={}
-      ></textarea>
-      <select> 
-        <option value='JS'> JS </option>
-        <option value='TS'> TS </option>
-        <option value='Python'> Python </option>
-      </select>
-      <button onClick={() => {handleGetAll(); handleAddSnippet()}}> SUBMIT </button>
+      <div className='input_box'>
+        <input
+          value={newTitle}
+          placeholder='insert title here!'
+          onChange={e => setNewTitle(e.target.value)}
+        ></input>
+        <textarea
+          value={newDescription}
+          placeholder='paste code snippet here!'
+          onChange={e => setNewDescription(e.target.value)}
+
+        ></textarea>
+        <select value={newLanguage} onChange={e => setNewLanguage(e.target.value)}>
+          <option value='JS'> JS </option>
+          <option value='TS'> TS </option>
+          <option value='Python'> Python </option>
+        </select>
+        <button onClick={() => { handleGetAll(); handleAddSnippet() }}> SUBMIT </button>
       </div>
 
       <div className='list'>
@@ -73,9 +124,15 @@ function App() {
         </li>
 
       </div>
+      <div className='chosenSnippet'>
+        <h1>{chosenSnippet.title}</h1>
+        <p>{chosenSnippet.description}</p>
+        <p>{chosenSnippet.language}</p>
+      </div>
     </div>
   );
 }
+
 
 
 
